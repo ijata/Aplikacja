06d19e026592d53876319842ae0dd01f5f071294
ijata <anowi.enohp@gmail.com>
2024-01-05 17:03:36 +0100

    Dodaj pliki projektów.

---
+++ Aplikacja/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Aplikacja
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Aplikacja/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using Aplikacja.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MenuViewCommand { get; set; }
+        public RelayCommand ChildrenViewCommand { get; set; }
+        public RelayCommand UserViewCommand { get; set; }
+        public RelayCommand MessageViewCommand { get; set; }
+        public RelayCommand PresenceViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public MenuViewModel MenuVM { get; set; }   
+        public ChildrenViewModel ChildrenVM { get; set; }
+        public UserViewModel UserVM { get; set; }
+        public MessageViewModel MessageVM { get; set; }
+        public PresenceViewModel PresenceVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }   
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }    
+            set { _currentView = value;
+                OnPropertyChanged();
+            }   
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            MenuVM = new MenuViewModel();
+            ChildrenVM = new ChildrenViewModel();   
+            UserVM = new UserViewModel();
+            MessageVM = new MessageViewModel();
+            PresenceVM = new PresenceViewModel();
+            SettingsVM = new SettingsViewModel();   
+            
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            MenuViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MenuVM;
+            });
+
+            ChildrenViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ChildrenVM;
+            });
+
+            UserViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = UserVM;
+            });
+            MessageViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MessageVM;
+            });
+            PresenceViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PresenceVM;
+            });
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
